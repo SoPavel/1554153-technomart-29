94dbd8f65e34f8ab2083343216a67e3d9d952cb1
SoPavel <pavellets1490@gmail.com>
2020-11-06 17:14:59 +0100

    Оживляем интерактивные элементы

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let btn = document.querySelector('.map-link');
+let modalLogin = document.querySelector('.modal-login');
+let modalClose = document.querySelector('.modal-close');
+let loginUser = document.querySelector('.login-user');
+let userEmail = document.querySelector('.user-e-mail');
+let loginForm = document.querySelector('.login-form');
+let textMessage = document.querySelector('.text-message');
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+btn.addEventListener("click", function (evt) {
+ evt.preventDefault();
+modalLogin.classList.add('modal-show');
+loginUser.focus();
+ if (storage) {
+    loginUser.value = storage;
+loginUser.focus();
+  } else {
+    loginEmail.focus();
+  }
+});
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalLogin.classList.remove("modal-show");
+modalLogin.classList.remove("modal-error");
+});
+loginForm.addEventListener("submit", function (evt) {
+if (!loginUser.value || !loginEmail.value) {
+    evt.preventDefault();
+modalLogin.classList.remove("modal-error");
+modalLogin.offsetWidth = modalLogin.offsetWidth;
+modalLogin.classList.add("modal-error");
+  } else {
+        if (isStorageSupport) {
+    localStorage.setItem("name", loginUser.value);
+      }
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalLogin.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modalLogin.classList.remove("modal-show");
+    }
+  }
+});
